5f3379aa8397c82938062db7218d9a10bc98558a
Mark Myers <mark.myers@aittech.com>
2021-04-07 19:32:20 +0000

    Tutorial morning session with stripe payments total

---
+++ models/marts/int_orders.sql
@@ -0,0 +1,32 @@
+with
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+payments as (
+
+    select * from {{ ref('stg_stripe__payments') }}
+    where status != 'fail'
+
+),
+
+joined as (
+
+    select
+    
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        sum(payments.amount) as order_total
+        
+    from orders
+    left join payments on orders.order_id = payments.order_id
+    group by 1,2,3
+
+)
+
+select * from joined
+
